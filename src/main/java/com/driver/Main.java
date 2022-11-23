b746ef20b64bd784a89fe9124e6af3dac4aefdac
Debojeet Chakraborty <17debojeet@gmail.com>
2022-11-23 14:26:54 +0530

    First Commit-Pizza

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,16 +2,16 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-    DeluxePizza dp = new DeluxePizza(true);
+    DeluxePizza dp = new DeluxePizza(false);
     System.out.println(dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
-    dp.addTakeaway();
-    System.out.println(dp.getBill());
+      dp.addTakeaway();
+      System.out.println(dp.getBill());
 
-    Pizza p = new Pizza(true);
-    p.addExtraCheese();
-    p.addExtraToppings();
+    Pizza p = new Pizza(false);
+   // p.addExtraCheese();
+  //  p.addExtraToppings();
     p.addTakeaway();
     System.out.println(p.getBill());
   }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,21 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int base;
+    private int ec;
+    private int ta;
+    private int top;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(this.isVeg){
+            this.price+=300;
+            this.base=300;
+        }
+        else{
+            this.price+=400;this.base=400;
+        }
     }
 
     public int getPrice(){
@@ -17,18 +28,52 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
-    }
+        this.ec+=80;
+        this.price+=80;
+        }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg){
+            this.top+=70;
+            this.price+=70;
+        }
+        else{
+            this.top+=120;
+            this.price+=120;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        this.ta=20;
+        this.price+=20;
     }
+    ///Base Price Of The Pizza: 300
+    //Extra Cheese Added: 80
+    //Extra Toppings Added: 70
+    //Paperbag Added: 20
+    //Total Price: 470
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+     String s="";
+     if(isVeg){
+        s+="Base Price Of The Pizza: "+300+"\n";
+     }
+     else{
+         s+="Base Price Of The Pizza: "+400+"\n";
+     }
+     if(this.ec>0){
+         s+="Extra Cheese Added: "+this.ec+"\n";
+     }
+     if(this.top>0){
+         s+="Extra Toppings Added: "+this.top+"\n";
+     }
+     if(this.ta>0){
+         s+="Paperbag Added: "+this.ta+"\n";
+     }
+     s+="Total Price: "+this.price+"\n";
+     return s;
     }
 }
